a567f49357f407b39a4cd1b3c58bf7ce3cea829c
Milan Khadka <khadkamilan6969@gmail.com>
2024-12-17 19:24:52 +0545

    Initial commit - Empty MAUI project

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseWorkMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseWorkMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
